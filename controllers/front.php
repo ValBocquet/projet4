56f6a8cfb6f1e87490f2478dc85c31a3302a1e39
ValBocquet <valentin.bocquet9@gmail.com>
2019-04-29 14:49:43 +0200

    creation class pour le model

--- controllers/front.php
+++ controllers/front.php
@@ -2,20 +2,24 @@
 require 'model/front.php';
 
 function home() {
-    $posts = getPosts();
+    $Manager = new Manager();
+    $posts = $Manager->getPosts();
     require 'views/home.php';
 }
 
 function articleOnly() {
     if(isset($_GET['id']) && $_GET['id'] > 0) {
-        $post = getPost($_GET['id']);
-        $comments = getComments($_GET['id']);
+        $Manager = new Manager();
+
+        $post = $Manager->getPost($_GET['id']);
+        $comments = $Manager->getComments($_GET['id']);
         require 'views/article.php';
     }
 }
 
 function addComment($postID, $name, $message) {
-    $affectedLines = postComment($postID, $name, $message);
+    $Manager = new Manager();
+    $affectedLines = $Manager->postComment($postID, $name, $message);
 
     if ($affectedLines === false) {
         die('Impossible d\'ajouter le commentaire');
--- model/front.php
+++ model/front.php
@@ -1,32 +1,38 @@
 <?php
 require 'database.php';
 
-
-    function getPosts() {
+class Manager
+{
+    function getPosts()
+    {
         $pdo = connectionBdd();
         $req = $pdo->query('SELECT * FROM articles ORDER BY id DESC LIMIT 0,5');
         return $req;
     }
 
-    function getPost($idGet) {
+    function getPost($idGet)
+    {
         $pdo = connectionBdd();
         $req = $pdo->prepare('SELECT * FROM articles WHERE id= :id');
         $req->execute(['id' => $idGet]);
         $post = $req->fetch();
         return $post;
     }
-    function getComments($postID) {
+
+    function getComments($postID)
+    {
         $pdo = connectionBdd();
         $comments = $pdo->prepare('SELECT * FROM commentaires WHERE article_referent= :article_referent ORDER BY date_publish_comm DESC');
         $comments->execute(['article_referent' => $postID]);
         return $comments;
     }
 
-    function postComment($postID, $name, $message) {
+    function postComment($postID, $name, $message)
+    {
         $pdo = connectionBdd();
         $comments = $pdo->prepare('INSERT INTO commentaires(article_referent, date_publish_comm, name, message) VALUES
 (?, NOW(),? ,?)');
         $affectedLines = $comments->execute(array($postID, $name, $message));
         return $affectedLines;
     }
-
+}
