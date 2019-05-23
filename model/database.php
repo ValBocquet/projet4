873dca6d0729968286a4ff8c5880459b838297e9
ValBocquet <valentin.bocquet9@gmail.com>
2019-05-23 19:21:37 +0200

    creation du .zip pour la soutenance P4

--- model/database.php
+++ model/database.php
@@ -1,8 +1,8 @@
 <?php
 class Manager {
     function connectionBdd() {
-        $pdo = new PDO('mysql:dbname=blog; host=localhost', 'root', 'root');
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo = new PDO('mysql:dbname=blog; host=localhost', 'root', '');
+        // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
 }
--- valeucjd_blog.sql
+++ valeucjd_blog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 07 mai 2019 à 15:54
--- Version du serveur :  5.7.24
--- Version de PHP :  7.2.14
+-- Hôte : localhost:3306
+-- Généré le :  jeu. 23 mai 2019 à 19:18
+-- Version du serveur :  10.2.24-MariaDB
+-- Version de PHP :  7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `blog`
+-- Base de données :  `valeucjd_blog`
 --
 
 -- --------------------------------------------------------
@@ -28,14 +28,12 @@ SET time_zone = "+00:00";
 -- Structure de la table `articles`
 --
 
-DROP TABLE IF EXISTS `articles`;
-CREATE TABLE IF NOT EXISTS `articles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `date_upload` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+  `date_upload` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `articles`
@@ -43,7 +41,8 @@ CREATE TABLE IF NOT EXISTS `articles` (
 
 INSERT INTO `articles` (`id`, `title`, `content`, `date_upload`) VALUES
 (5, 'Un fou de la night', '<p><strong>lorem lorem lorem lor</strong>em lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem</p>', '2019-04-30 11:07:29'),
-(7, 'Essai d\'un nouvel article', '<p>Salut, les enfants je suis un gentil monsieur...</p>', '2019-05-01 18:14:43');
+(7, 'Essai d\'un nouvel article', '<p>Salut, les enfants je suis un gentil monsieur...</p>', '2019-05-01 18:14:43'),
+(9, 'Essai d\'article', '<p>Je suis un <strong>article</strong></p>', '2019-05-23 15:57:08');
 
 -- --------------------------------------------------------
 
@@ -51,16 +50,14 @@ INSERT INTO `articles` (`id`, `title`, `content`, `date_upload`) VALUES
 -- Structure de la table `commentaires`
 --
 
-DROP TABLE IF EXISTS `commentaires`;
-CREATE TABLE IF NOT EXISTS `commentaires` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `commentaires` (
+  `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `message` text NOT NULL,
   `date_publish_comm` datetime NOT NULL,
   `article_referent` int(11) NOT NULL,
-  `danger` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+  `danger` tinyint(1) NOT NULL DEFAULT 0
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `commentaires`
@@ -68,10 +65,46 @@ CREATE TABLE IF NOT EXISTS `commentaires` (
 
 INSERT INTO `commentaires` (`id`, `name`, `message`, `date_publish_comm`, `article_referent`, `danger`) VALUES
 (1, 'Valentin', 'Je suis un super commentaire', '2019-04-28 09:30:29', 5, 1),
-(2, 'Valentin', '<p>Voici un super novueau <strong>commena</strong> pour l\'article 2</p>', '2019-04-28 15:39:38', 5, 1),
+(13, 'asas', 'dzdzdazd', '2019-05-23 15:56:55', 9, 1),
+(14, 'coucou', 'dzlkdzdka ', '2019-05-23 16:52:14', 9, 1),
 (7, 'Valou', 'Commentaire non signalé !', '2019-05-02 21:14:21', 5, 0),
-(9, 'Guy', 'Je suis un nouveau commentaire.', '2019-05-05 09:42:03', 7, 0),
-(8, 'Val', 'J\'adore cet article ! Bravo !', '2019-05-03 17:06:14', 7, 0);
+(9, 'Guy', 'Je suis un nouveau commentaire.', '2019-05-05 09:42:03', 7, 1),
+(8, 'Val', 'J\'adore cet article ! Bravo !', '2019-05-03 17:06:14', 7, 0),
+(10, 'Valentin', 'Voici le premier commentaire de production !', '2019-05-12 18:03:56', 7, 0),
+(11, 'Valentin', 'Dernier essai de post commentaire.', '2019-05-17 18:45:54', 5, 0),
+(15, 'Valentin', 'Commentaire de ouf ', '2019-05-23 16:52:54', 9, 0);
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
